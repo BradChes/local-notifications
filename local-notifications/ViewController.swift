6b6e4152469468a442cc74fb79722ec8823e896e
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-05-29 14:34:18 +0100

    Added UserNotifications to an application, this enables local notifications to come through

--- local-notifications/ViewController.swift
+++ local-notifications/ViewController.swift
@@ -6,15 +6,85 @@
 //  Copyright © 2020 Brad Chesworth. All rights reserved.
 //
 
+import UserNotifications
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    private var shouldRemind = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yes")
+            } else {
+                print("No")
+            }
+        }
+    }
 
+    @objc func scheduleLocal() {
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+        center.removeAllDeliveredNotifications()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .defaultCritical
+        
+// Shows an implementation of a trigger that will happen depending on a date components
+//        var dateComponents = DateComponents()
+//        dateComponents.hour = 10
+//        dateComponents.minute = 30
+//        let trigger  = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let trigger = !shouldRemind ? UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) : UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let remind = UNNotificationAction(identifier: "remind", title: "Remind me later")
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remind], intentIdentifiers:[])
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        if let customData = userInfo["customData"] as? String {
+            print("custom data received: \(customData)")
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                print("Default identifier")
+            case "show":
+                print("Show more information...")
+            case "remind":
+                print("Remind me later...")
+                shouldRemind = true
+                scheduleLocal()
+                shouldRemind = false
+            default:
+                break
+            }
+        }
+        
+        completionHandler()
+    }
 }
 
